30396f21e2a6210849b72140dd4790ab184d2511
argo-rich <rich@argohaus.com>
2024-12-10 12:32:19 -0600

    Updated unit test

--- Northwind.UnitTests/UserManagementTests.cs
+++ Northwind.UnitTests/UserManagementTests.cs
@@ -6,9 +6,18 @@ namespace Northwind.UnitTests;
 public class UserManagementTests
 {
     [Fact]
-    public void Password_Encrypts_and_Compares()
+    public void Password_Encrypts_and_Compares_25()
     {
         string password = "password10thisissten12345";
+        string passwdEncrypted = CreateHash(password);
+
+        Assert.True(ValidatePassword(password, passwdEncrypted));
+    }
+
+    [Fact]
+    public void Password_Encrypts_and_Compares_8()
+    {
+        string password = "P@ssw/8c";
         string passwdEncrypted = CreateHash(password);
 
         Assert.True(ValidatePassword(password, passwdEncrypted));
