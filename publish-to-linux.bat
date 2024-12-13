708df88817eceed72c9b97c92188864ddbeb0921
argo-rich <rich@argohaus.com>
2024-12-13 13:05:13 -0600

    NWND-18 - Clean up after potentially fixing issue with Jenkins build

--- publish-to-linux.bat
+++ publish-to-linux.bat
@@ -1,8 +1,12 @@
-REM Run the dotnet publish command to build the artifacts and deploys them to production
+REM Run the dotnet publish command and create publish.tar.bz2
 cd dotnet-webapp-ec2
 dotnet publish -o publish --os linux
 cd publish
 tar cjvf ..\publish.tar.bz2 .
+
+REM scp publish.tar.bz2 to the server
 cd ..
-scp -Bq -i %NORTHWIND_KEY% .\publish.tar.bz2 ec2-user@ec2-34-227-151-78.compute-1.amazonaws.com:~/dotnet-webapp-ec2
-ssh -q -i %NORTHWIND_KEY% ec2-user@ec2-34-227-151-78.compute-1.amazonaws.com "source /home/ec2-user/.bash_profile && /home/ec2-user/scripts/deploy-publish-file.sh"
+scp -i %NORTHWIND_KEY% .\publish.tar.bz2 ec2-user@ec2-34-227-151-78.compute-1.amazonaws.com:~/dotnet-webapp-ec2
+
+REM run the deploy script on the server
+ssh -i %NORTHWIND_KEY% ec2-user@ec2-34-227-151-78.compute-1.amazonaws.com "source /home/ec2-user/.bash_profile && /home/ec2-user/scripts/deploy-publish-file.sh"
