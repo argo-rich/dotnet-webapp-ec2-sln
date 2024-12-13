9b5ae2efbd661924791789c1fa0cd836624b3983
argo-rich <rich@argohaus.com>
2024-12-12 20:58:33 -0600

    NWND-18 - putting ssh into quiet mode to hopefully prevent Jenkins from timing out

--- publish-to-linux.bat
+++ publish-to-linux.bat
@@ -5,4 +5,4 @@ cd publish
 tar cjvf ..\publish.tar.bz2 .
 cd ..
 scp -Bq -i %NORTHWIND_KEY% .\publish.tar.bz2 ec2-user@ec2-34-227-151-78.compute-1.amazonaws.com:~/dotnet-webapp-ec2
-ssh -i %NORTHWIND_KEY% ec2-user@ec2-34-227-151-78.compute-1.amazonaws.com "source /home/ec2-user/.bash_profile && /home/ec2-user/scripts/deploy-publish-file.sh"
+ssh -q -i %NORTHWIND_KEY% ec2-user@ec2-34-227-151-78.compute-1.amazonaws.com "source /home/ec2-user/.bash_profile && /home/ec2-user/scripts/deploy-publish-file.sh"
