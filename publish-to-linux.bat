dad6e7b21ef11a44c1027a7a2d1f773afeaf7480
argo-rich <rich@argohaus.com>
2024-12-12 20:18:56 -0600

    NWND-18 - Deploys files to EC2 instance and restarts webapp

--- publish-to-linux.bat
+++ publish-to-linux.bat
@@ -1,3 +1,8 @@
-REM Publishes the webapp project to a folder named "publish"
+REM Run the dotnet publish command to build the artifacts and deploys them to production
 cd dotnet-webapp-ec2
 dotnet publish -o publish --os linux
+cd publish
+tar cjvf ..\publish.tar.bz2 .
+cd ..
+scp -i %NORTHWIND_KEY% .\publish.tar.bz2 ec2-user@ec2-34-227-151-78.compute-1.amazonaws.com:~/dotnet-webapp-ec2
+ssh -i %NORTHWIND_KEY% ec2-user@ec2-34-227-151-78.compute-1.amazonaws.com "source /home/ec2-user/.bash_profile && /home/ec2-user/scripts/deploy-publish-file.sh"
